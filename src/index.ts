2a20f1df881bfff96b830d23ceb657f801d34467
Lutz Schwemer Panchez <shark@shark5060.net>
2026-02-25 13:08:10 +0100

    Add rate limiting for static asset routes

--- src/index.ts
+++ src/index.ts
@@ -69,10 +69,16 @@ app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 const STATIC_ROOT = process.cwd();
-app.get('/favicon.ico', (_req, res) => {
+const staticAssetLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 300,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+app.get('/favicon.ico', staticAssetLimiter, (_req, res) => {
   res.sendFile(path.join(STATIC_ROOT, 'favicon.ico'));
 });
-app.get('/branding/feathers.png', (_req, res) => {
+app.get('/branding/feathers.png', staticAssetLimiter, (_req, res) => {
   res.sendFile(path.join(STATIC_ROOT, 'feathers.png'));
 });
 
